c6dd8e21462286c1008beea842172b9b6648f194
Nguyen Minh Tri <minhtri23092003@gmail.com>
2021-08-26 20:36:01 +0700

    tao va luu 3 file

---
+++ timphuongtrinhbac2.py
@@ -0,0 +1,9 @@
+#a*x^2+b*x+c=0
+print("Start")
+a = input("Nhap a: ")
+b = input("Nhap b: ")
+c=input("Nhap c: ")
+delta=b**2-4*a*c
+if delta >=0 print ("x1=(-b+sqrt(delta))/2*a"and "x2=(-b-sqrt(delta))/2*a")
+if deta <=0 print("phuong trinh vo nghiem")
+if delta=0 print("x1=x2=(-b)/2*a")
